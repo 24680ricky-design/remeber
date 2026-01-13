65fab7f670d8e7b914990d9fb32697759ae91f5e
ricky <24680ricky@gmail.com>
2026-01-13 15:20:14 +0800

    chore: migrate to local tailwind, add debug logs, clean formatting

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                nordic: {
+                    cream: '#fdfbf7',
+                    pink: '#e8d5d5',
+                    green: '#8da399',
+                    blue: '#8fa3ad',
+                    text: '#4a4a4a',
+                    card: '#ffffff'
+                }
+            },
+            fontFamily: {
+                sans: ['Inter', 'sans-serif'],
+            },
+            borderRadius: {
+                '4xl': '2rem',
+            }
+        },
+    },
+    plugins: [],
+}
